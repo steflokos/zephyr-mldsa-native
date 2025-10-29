de3b121c4193f224c2b615cc60c06420a7fafd84
willieyz <willie.zhao@chelpis.com>
2025-10-29 18:30:59 +0800

    HOL-Light: Allow interactive use of hol.sh in the hol_light shell
    - Ported from mlkem-native PR #1244
    - Previously, while compiling and running the HOL-Light proofs in
      `nix develop .#hol_light` worked, trying to start the interactive
      shell `hol.sh` for proof development would fail. This required
      proof developers to install HOL-Light and s2n-bignum locally,
      both inconvenient and error proone because one would need to keep
      that local version and the nix version in sync.
    
    - This commit adjusts the nix flake to setup HOL-Light in such a way
      that hol.sh can be used interactively inside the `hol_light` shell.
      You can test  for this PR with following in nix hol_light shell:
    
    ```
       hol.sh
    ```
    
      Now, when you enter the shell via `nix develop .#hol_light`, the
      interactive shell `hol.sh` is in the PATH, and within it, both
      s2n-bignum and proofs/hol_light/arm are added to HOL-Light's own
      search path (`load_path`).
    
    - Note that mldsa-native currently does not have any HOL-Light proofs.
      Therefore, we have not ported the HOL-Light interactive shell tests
      to mldsa-native.
    
    Signed-off-by: willieyz <willie.zhao@chelpis.com>

--- nix/hol_light/default.nix
+++ nix/hol_light/default.nix
@@ -2,7 +2,7 @@
 # Copyright (c) The mldsa-native project authors
 # SPDX-License-Identifier: Apache-2.0 OR ISC OR MIT
 
-{ hol_light, fetchFromGitHub, writeText, ... }:
+{ hol_light, fetchFromGitHub, writeText, ocamlPackages, ledit, ... }:
 hol_light.overrideAttrs (old: {
   setupHook = writeText "setup-hook.sh" ''
     export HOLDIR="$1/lib/hol_light"
@@ -15,8 +15,11 @@ hol_light.overrideAttrs (old: {
     rev = "0e4b1bd8c7d400214d6fa6027f15a4221b54f8d4";
     hash = "sha256-M6ddzqoAFyMBmaznuz31+o035xdEz4VXZMHhH4Dm4c8=";
   };
-  patches = [ ./0005-Fix-hollight-path.patch ];
-  propagatedBuildInputs = old.propagatedBuildInputs ++ old.nativeBuildInputs;
+  patches = [
+    ./0005-Configure-hol-sh-for-mldsa-native.patch
+    ./0006-Add-findlib-to-ocaml-hol.patch
+  ];
+  propagatedBuildInputs = old.propagatedBuildInputs ++ old.nativeBuildInputs ++ [ ocamlPackages.pcre2 ledit ];
   buildPhase = ''
     HOLLIGHT_USE_MODULE=1 make hol.sh
     patchShebangs hol.sh
